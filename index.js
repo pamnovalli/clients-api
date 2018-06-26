3f2f6beb063295268012f59567d8de5b606a8da7
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-06-26 11:50:03 -0300

    Criar API de cadastro de cliente

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectID = require('mongodb').ObjectID;
+
+const app = express();
+
+app.use(expressMongoDb('mongodb://loja:loja123@165.227.221.155/loja'));
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/cadastrocliente', (req, res) => {
+    let cadastrocliente = {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        endereço: req.body.endereço,
+        telefone: req.body.telefone
+    };
+
+    req.db.collection('loja').insert(cadastrocliente, (err) => {
+        if(err){
+            res.status(500).send();
+            return;
+        }
+    
+        res.send(req.body);
+    });
+
+});
+
+app.put('/cadastrocliente/:cpf', (req, re) => {
+    let query = {
+        _cpf: ObjectID(req.params.cpf)
+    };
+
+    let cadastrocliente = {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        endereço: req.body.endereço,
+        telefone: req.body.telefone
+    };
+
+    req.db.collection('loja').updateOne(query, cadastrocliente, (err, data) => {
+        if(err){
+            res.status(500).send();
+            return;
+        }
+    
+        res.send(data);
+    });
+});
+
+app.get('/cadastrocliente', (req, res) => {
+    req.db.collection('loja').find().toArray((err, data) => {
+        if(err){
+            res.status(500).send();
+            return;
+        }
+        res.send(data);
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Aplicação iniciada'));
